00d639c564c3b926fec26702c548020a72b81b52
Jonas Souza <Jonassouza305@yahoo.com.br>
2024-01-31 12:30:08 -0300

    adicionei o primeiro endpoint para buscar uma lista de filmes, esse endpoint  possuí dois parâmetros de consulta, filtrar por `language` e burcar de forma ordenada tanto por titulo quanto por data ex: `?sort=title`

--- src/server.ts
+++ src/server.ts
@@ -6,8 +6,55 @@ const app = express();
 const prisma = new PrismaClient();
 
 app.get("/movies", async (req, res) => {
-  const movies = await prisma.movie.findMany();
-  res.json(movies);
+  const { sort } = req.query;
+  const { language } = req.query;
+  const languageName = language as string;
+
+  let where = {};
+  if (languageName) {
+    where = {
+      languages: {
+        name: {
+          equals: languageName,
+          mode: "insensitive",
+        },
+      },
+    };
+  }
+
+  let orderBy = {};
+  if (sort === "title") {
+    orderBy = {
+      title: "asc",
+    };
+  } else if (sort === "release_date") {
+    orderBy = {
+      release_date: "asc",
+    };
+  }
+
+  try {
+    const movies = await prisma.movie.findMany({
+      where: where,
+      orderBy,
+      include: {
+        genres: true,
+        languages: true,
+      },
+    });
+    const allMovies = movies.length;
+    let sumDurationsAllmovies = 0;
+    for (let movie of movies) {
+      sumDurationsAllmovies += movie.duration ? movie.duration : 0;
+    }
+    const averageLengthOfFilms =
+      allMovies > 0 ? Math.round(sumDurationsAllmovies / allMovies) : 0;
+    res.status(200).json({ allMovies, averageLengthOfFilms, movies });
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: "There was a problem searching for movies." });
+  }
 });
 
 app.listen(port, () => {
