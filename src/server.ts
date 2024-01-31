543e6ebdaacef9268c85158d7fcb1498e05457ad
Jonas Souza <Jonassouza305@yahoo.com.br>
2024-01-31 18:58:12 -0300

    instalei os pacotes `swagger-ui-express` e `@types/swagger-ui-express`,criei o arquivo `swagger.json` na raíz e configurei o swagger para documentar a API . criei as rotas restantes no ``server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swagger.json";
 
 const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(express.json());
 
 app.get("/movies", async (req, res) => {
   const { sort } = req.query;
@@ -53,7 +58,179 @@ app.get("/movies", async (req, res) => {
   } catch (error) {
     res
       .status(500)
-      .send({ message: "There was a problem searching for movies." });
+      .json({ message: "There was a problem searching for movies." });
+  }
+});
+app.post("/movies", async (req, res) => {
+  const data = { ...req.body };
+
+  try {
+    const queryDuplicateMovieByTitle = await prisma.movie.findFirst({
+      where: {
+        title: { equals: data.title, mode: "insensitive" },
+      },
+    });
+    if (queryDuplicateMovieByTitle) {
+      return res
+        .status(409)
+        .json({ message: "duplicate film, it is not possible to register" });
+    }
+    data.release_date = data.release_date
+      ? new Date(data.release_date)
+      : undefined;
+
+    await prisma.movie.create({
+      data,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Failed to register movie" });
+  }
+  res.status(201).send({ message: "film registered successfully" });
+});
+
+app.put("/movies/:id", async (req, res) => {
+  const id = Number(req.params.id);
+  const data = { ...req.body };
+  try {
+    const existFilm = await prisma.movie.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!existFilm) {
+      return res.status(404).json({ message: "this film doesn't exist" });
+    }
+    data.release_date = data.release_date
+      ? new Date(data.release_date)
+      : undefined;
+
+    await prisma.movie.update({
+      where: { id },
+      data,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "error when changing data" });
+  }
+
+  res.status(200).send({ message: "movie alteration in sucefull" });
+});
+
+app.delete("/movies/:id", async (req, res) => {
+  const id = Number(req.params.id);
+
+  try {
+    const existFilm = await prisma.movie.findUnique({
+      where: { id },
+    });
+    if (!existFilm) {
+      return res.status(404).json({ message: "film does not exist" });
+    }
+    await prisma.movie.delete({
+      where: { id },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Failed to delete the movie" });
+  }
+  res.status(200).send({ message: "Successfully deleted movie" });
+});
+
+app.get("/movies/:genrename", async (req, res) => {
+  const { genrename } = req.params;
+  try {
+    const movieByGenre = await prisma.movie.findMany({
+      where: {
+        genres: {
+          name: {
+            equals: genrename,
+            mode: "insensitive",
+          },
+        },
+      },
+      include: {
+        genres: true,
+        languages: true,
+      },
+    });
+    res.status(200).json({ movieByGenre });
+  } catch (error) {
+    res.status(500).json({ message: "error when filtering movie by genre" });
+  }
+});
+
+app.put("/genres/:id", async (req, res) => {
+  const id = Number(req.params.id);
+  const { name } = req.body;
+  try {
+    const existGenre = await prisma.genre.findUnique({
+      where: { id },
+    });
+    if (!existGenre) {
+      return res.status(404).json({ message: "this genre doesn't exist" });
+    }
+    await prisma.genre.update({
+      where: { id },
+      data: { name },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "error when changing data" });
+  }
+  res.status(200).json({ message: "genre alteration in sucefull" });
+});
+
+app.post("/genres", async (req, res) => {
+  const { name } = req.body;
+  try {
+    const queryDuplicateGenreByName = await prisma.genre.findFirst({
+      where: {
+        name: { equals: name, mode: "insensitive" },
+      },
+    });
+
+    if (queryDuplicateGenreByName) {
+      return res
+        .status(409)
+        .json({ message: "duplicate genre, it is not possible to register" });
+    }
+
+    await prisma.genre.create({
+      data: { name },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Failed to register genre" });
+  }
+
+  res.status(201).send({ message: "genre registered successfully" });
+});
+
+app.get("/genres", async (req, res) => {
+  try {
+    const genres = await prisma.genre.findMany();
+
+    res.status(200).json({ genres });
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: "There was a problem searching for genres." });
+  }
+});
+app.delete("/genres/:id", async (req, res) => {
+  const id = Number(req.params.id);
+  try {
+    const existGenre = await prisma.genre.findUnique({
+      where: { id },
+    });
+    if (!existGenre) {
+      return res.status(404).json({ message: "genre does not exist" });
+    }
+
+    await prisma.genre.delete({
+      where: { id },
+    });
+
+    res.status(200).send({ message: "Successfully deleted genre" });
+  } catch (error) {
+    res.status(500).json({ message: "Failed to delete the genre" });
   }
 });
 
